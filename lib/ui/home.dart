788b636c1bdcef9b298c64391d32503affc53d5a
Emmanuelagbo-4 <agboemmanuel360@gmail.com>
2020-04-11 17:50:22 +0100

    Added OrderButton

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -6,19 +8,99 @@ class Home extends StatelessWidget {
     return Center(
       child: Container(
       alignment: Alignment.center,
+      padding: EdgeInsets.only(top:30.0, left:10.0),
       color: Colors.deepOrangeAccent,
       
-      child: Text("Pizza", 
+      child: Column(children: <Widget>[
+        Row(children: <Widget>[
+         Text("Magherita", 
       textDirection: TextDirection.ltr,
       style: TextStyle(
-        fontSize: 80.0,
+        fontSize: 30.0,
+        decoration: TextDecoration.none,
+        fontFamily: 'Oxygen',
+        fontWeight: FontWeight.bold,
+      ),
+      ),
+       Expanded(child:Text("Tomato, Mozerrella, Basil", 
+      textDirection: TextDirection.ltr,
+      style: TextStyle(
+        fontSize: 20.0,
         decoration: TextDecoration.none,
         fontFamily: 'Oxygen',
         fontWeight: FontWeight.bold,
       ),
       )
+      )
+      ],
+      ),
+      Row(children: <Widget>[
+         Text("Marinara", 
+      textDirection: TextDirection.ltr,
+      style: TextStyle(
+        fontSize: 30.0,
+        decoration: TextDecoration.none,
+        fontFamily: 'Oxygen',
+        fontWeight: FontWeight.bold,
+      ),
+      ),
+       Expanded(child:Text("Tomato, Garlic", 
+      textDirection: TextDirection.ltr,
+      style: TextStyle(
+        fontSize: 20.0,
+        decoration: TextDecoration.none,
+        fontFamily: 'Oxygen',
+        fontWeight: FontWeight.bold,
+      ),
+      )
+      )
+      ],
+      ),
+      PizzaImageWidget(),
+      OrderButton(),
+      ],)
+      
+      
     )
     );
   }
 
 }
+
+class PizzaImageWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    AssetImage pizzaAsset = AssetImage('images/Pizza.jpg');
+    Image image = Image(image: pizzaAsset, width: 400, height: 200,);
+    return Container(child: image);
+  }
+
+}
+
+ class OrderButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var button = Container(
+      margin: EdgeInsets.only(top:50.0),
+      child: RaisedButton(
+        child: Text("Order Your Pizza"),
+        color: Colors.lightGreen,
+        elevation: 5.0,
+        onPressed: () {
+          order(context);
+        },
+        ),
+        );
+        return Container(child: button,);
+  }
+  
+   void order(BuildContext context) {
+     var alert = AlertDialog(
+       title: Text("Order Completed"),
+       content: Text("Thanks for your order"),
+       );
+       showDialog(
+         context: context,
+         builder: (BuildContext context) => alert);
+   }
+ }
